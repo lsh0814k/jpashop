de831fe976b3355f66c0b0ff3b28b146fc987104
이상훈 <slee@i-magicsoft.com>
2023-12-03 15:41:24 +0900

    회원 웹 계층 추가

---
+++ src/main/java/jpabook/jpashop/web/MemberController.java
@@ -0,0 +1,53 @@
+package jpabook.jpashop.web;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping(value = "/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(@Validated MemberForm form, BindingResult result) {
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Member member = Member.builder()
+                .name(form.getName())
+                .address(new Address(form.getCity(), form.getStreet(), form.getZipcode()))
+                .build();
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/members")
+    public String members(Model model) {
+        List<Member> members = memberService.findAll();
+        model.addAttribute("members", members.stream().map(MemberResponse::new).collect(Collectors.toList()));
+
+        return "members/memberLisr";
+    }
+}
---
+++ src/main/java/jpabook/jpashop/web/MemberResponse.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop.web;
+
+import jpabook.jpashop.domain.Member;
+import lombok.Getter;
+
+@Getter
+public class MemberResponse {
+
+    public MemberResponse(Member member) {
+        this.name = member.getName();
+        this.city = member.getAddress().getCity();
+        this.street = member.getAddress().getStreet();
+        this.zipcode = member.getAddress().getZipcode();
+    }
+
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
+}
