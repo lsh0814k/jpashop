bda64180d53bccebd273e3fe8104a41946b43849
이상훈 <slee@i-magicsoft.com>
2023-12-03 14:09:11 +0900

    equals, hashcode 추가

--- src/main/java/jpabook/jpashop/domain/Quantity.java
+++ src/main/java/jpabook/jpashop/domain/Quantity.java
@@ -5,6 +5,8 @@ import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 import static lombok.AccessLevel.PROTECTED;
 
 @Getter
@@ -31,5 +33,18 @@ public class Quantity {
         }
 
         return new Quantity(rest);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Quantity quantity = (Quantity) o;
+        return getValue() == quantity.getValue();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getValue());
     }
 }
